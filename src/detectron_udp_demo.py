2941357644742f0275405b19d9c4e063512d8a3b
Kale, Niraj <niraj.kale@Nirajs-Macbook.local>
2024-05-05 22:26:49 +0530

    added udp streaming demo

--- src/detectron_udp_demo.py
+++ src/detectron_udp_demo.py
@@ -1,14 +1,16 @@
 """Uses src/detectron to detect objects in a webcam feed."""
 
+import os
 import cv2
 import numpy as np
 from utils.gst_utils import *
 import time
+from rich.console import Console, Text
 
 if __package__:
-    from .detectron import BaseDetector, ONNXDetector
+    from .detectron import BaseDetector, TensorRTDetector
 else:
-    from detectron import BaseDetector, ONNXDetector
+    from detectron import BaseDetector, TensorRTDetector
 
 
 if __name__ == "__main__":
@@ -17,7 +19,14 @@ if __name__ == "__main__":
     HEIGHT = 640
     FRAME_RATE = 10
     HOST_IP_ADDR = "192.168.3.2"
+    os.environ["LOG_LEVEL"] = "DEBUG"
+    model_path = r"/home/niraj/projects/laika/models/det-5n-half.engine"
+    classes = ["face", "human"]
 
+    console = Console(emoji=True)
+    detector = TensorRTDetector(
+        model_path=model_path, classes=classes, device=0, console=console
+    )
     reader_pipeline_str = reader_pipeline(
         flip_method=0,
         capture_width=WIDTH,
@@ -56,8 +65,9 @@ if __name__ == "__main__":
             new_frame_time = time.time()
             fps = round(1 / (new_frame_time - prev_frame_time), 2)
             prev_frame_time = new_frame_time
+            img1, detections = detector.predict(source=frame, isBGR=True)
             frame = cv2.putText(
-                frame,
+                img1,
                 f"FPS: {fps:.2f}",
                 (10, 50),
                 cv2.FONT_HERSHEY_SIMPLEX,
